bf9548d7fee2dc83e076d86ee3e4445271c33d92
Thomas Werkmeister <thomas@twerkmeister.com>
2019-07-29 19:32:52 +0200

    slimtaco model

---
+++ models/slimtaco.py
@@ -0,0 +1,128 @@
+from math import sqrt
+from torch import nn
+
+from layers.slim_gst_layers import GST
+from layers.slimtaco import Encoder, Decoder, Postnet
+from utils.generic_utils import sequence_mask
+
+
+# TODO: match function arguments with tacotron
+class SlimTaco(nn.Module):
+    """A smaller version of the tacotron 2 network."""
+    def __init__(self,
+                 num_chars,
+                 num_speakers,
+                 r,
+                 attn_win=False,
+                 attn_norm="softmax",
+                 prenet_type="original",
+                 prenet_dropout=True,
+                 forward_attn=False,
+                 trans_agent=False,
+                 forward_attn_mask=False,
+                 location_attn=True,
+                 separate_stopnet=True,
+                 use_gst=False,
+                 memory_size=5):
+        super(SlimTaco, self).__init__()
+        self.n_mel_channels = 80
+        self.n_frames_per_step = r
+        self.use_gst = use_gst
+        self.embedding_size = 256
+
+        self.embedding = nn.Embedding(num_chars, self.embedding_size)
+
+        std = sqrt(2.0 / (num_chars + self.embedding_size))
+        val = sqrt(3.0) * std  # uniform bounds for std
+        self.embedding.weight.data.uniform_(-val, val)
+        if num_speakers > 1:
+            self.speaker_embedding = nn.Embedding(num_speakers,
+                                                  self.embedding_size)
+            self.speaker_embedding.weight.data.normal_(0, 0.3)
+        self.encoder = Encoder(self.embedding_size)
+        if self.use_gst:
+            self.gst = GST(num_mel=self.n_mel_channels, num_heads=4,
+                           num_style_tokens=16,
+                           embedding_dim=self.embedding_size)
+        self.decoder = Decoder(self.embedding_size, self.n_mel_channels, r, attn_win,
+                               attn_norm, prenet_type, prenet_dropout,
+                               forward_attn, trans_agent, forward_attn_mask,
+                               location_attn, separate_stopnet, memory_size)
+        self.postnet = Postnet(self.n_mel_channels)
+
+    @staticmethod
+    def shape_outputs(mel_outputs, mel_outputs_postnet, alignments):
+        mel_outputs = mel_outputs.transpose(1, 2)
+        mel_outputs_postnet = mel_outputs_postnet.transpose(1, 2)
+        return mel_outputs, mel_outputs_postnet, alignments
+
+    def forward(self, text, text_lengths, mel_specs=None, speaker_ids=None):
+        # compute mask for padding
+        mask = sequence_mask(text_lengths).to(text.device)
+        embedded_inputs = self.embedding(text).transpose(1, 2)
+        encoder_outputs = self.encoder(embedded_inputs, text_lengths)
+        encoder_outputs = self._add_speaker_embedding(encoder_outputs,
+                                                      speaker_ids)
+        if self.use_gst and mel_specs is not None:
+            gst_outputs = self.gst(mel_specs)
+            gst_outputs = gst_outputs.expand(-1, encoder_outputs.size(1), -1)
+            encoder_outputs = encoder_outputs + gst_outputs
+
+        mel_outputs, stop_tokens, alignments = self.decoder(
+            encoder_outputs, mel_specs, mask)
+        mel_outputs_postnet = self.postnet(mel_outputs)
+        mel_outputs_postnet = mel_outputs + mel_outputs_postnet
+        mel_outputs, mel_outputs_postnet, alignments = self.shape_outputs(
+            mel_outputs, mel_outputs_postnet, alignments)
+        return mel_outputs, mel_outputs_postnet, alignments, stop_tokens
+
+    def inference(self, text, mel_specs=None, speaker_ids=None):
+        embedded_inputs = self.embedding(text).transpose(1, 2)
+        encoder_outputs = self.encoder.inference(embedded_inputs)
+        encoder_outputs = self._add_speaker_embedding(encoder_outputs,
+                                                      speaker_ids)
+        if self.use_gst and mel_specs is not None:
+            gst_outputs = self.gst(mel_specs)
+            gst_outputs = gst_outputs.expand(-1, encoder_outputs.size(1), -1)
+            encoder_outputs = encoder_outputs + gst_outputs
+
+        mel_outputs, stop_tokens, alignments = self.decoder.inference(
+            encoder_outputs)
+        mel_outputs_postnet = self.postnet(mel_outputs)
+        mel_outputs_postnet = mel_outputs + mel_outputs_postnet
+        mel_outputs, mel_outputs_postnet, alignments = self.shape_outputs(
+            mel_outputs, mel_outputs_postnet, alignments)
+        return mel_outputs, mel_outputs_postnet, alignments, stop_tokens
+
+    def inference_truncated(self, text, mel_specs=None, speaker_ids=None):
+        """Preserve model states for continuous inference."""
+
+        embedded_inputs = self.embedding(text).transpose(1, 2)
+        encoder_outputs = self.encoder.inference_truncated(embedded_inputs)
+        encoder_outputs = self._add_speaker_embedding(encoder_outputs,
+                                                      speaker_ids)
+        if self.use_gst and mel_specs is not None:
+            gst_outputs = self.gst(mel_specs)
+            gst_outputs = gst_outputs.expand(-1, encoder_outputs.size(1), -1)
+            encoder_outputs = encoder_outputs + gst_outputs
+
+        mel_outputs, stop_tokens, alignments = self.decoder.inference_truncated(
+            encoder_outputs)
+        mel_outputs_postnet = self.postnet(mel_outputs)
+        mel_outputs_postnet = mel_outputs + mel_outputs_postnet
+        mel_outputs, mel_outputs_postnet, alignments = self.shape_outputs(
+            mel_outputs, mel_outputs_postnet, alignments)
+        return mel_outputs, mel_outputs_postnet, alignments, stop_tokens
+
+    def _add_speaker_embedding(self, encoder_outputs, speaker_ids):
+        if hasattr(self, "speaker_embedding") and speaker_ids is None:
+            raise RuntimeError(" [!] Model has speaker embedding layer but speaker_id is not provided")
+        if hasattr(self, "speaker_embedding") and speaker_ids is not None:
+            speaker_embeddings = self.speaker_embedding(speaker_ids)
+
+            speaker_embeddings.unsqueeze_(1)
+            speaker_embeddings = speaker_embeddings.expand(encoder_outputs.size(0),
+                                                           encoder_outputs.size(1),
+                                                           -1)
+            encoder_outputs = encoder_outputs + speaker_embeddings
+        return encoder_outputs
--- utils/generic_utils.py
+++ utils/generic_utils.py
@@ -285,6 +285,22 @@ def setup_model(num_chars, num_speakers, c):
             separate_stopnet=c.separate_stopnet,
             use_gst=c.use_gst,
             memory_size=c.memory_size)
+    elif c.model.lower() == "slimtaco":
+        model = MyModel(
+            num_chars=num_chars,
+            num_speakers=num_speakers,
+            r=c.r,
+            attn_win=c.windowing,
+            attn_norm=c.attention_norm,
+            prenet_type=c.prenet_type,
+            prenet_dropout=c.prenet_dropout,
+            forward_attn=c.use_forward_attn,
+            trans_agent=c.transition_agent,
+            forward_attn_mask=c.forward_attn_mask,
+            location_attn=c.location_attn,
+            separate_stopnet=c.separate_stopnet,
+            use_gst=c.use_gst,
+            memory_size=c.memory_size)
     else:
         raise ValueError(f"unknown model type: {c.model.lower()}")
     return model
